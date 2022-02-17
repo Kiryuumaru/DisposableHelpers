fe9da410c27fdfae72709873b984419e69a3da3e
Kiryuumaru <kiryuumaru@outlook.com>
2022-02-18 05:01:25 +0800

    Added .net2.1

--- DisposableHelpers/Disposable.cs
+++ DisposableHelpers/Disposable.cs
@@ -5,21 +5,25 @@ using System.Threading.Tasks;
 namespace DisposableHelpers
 {
     /// <summary>
-    /// Contains all methods for performing proper disposable operations.
+    /// Contains all methods for performing proper <see cref="IDisposable"/> operations.
     /// </summary>
-    public abstract class Disposable :
-        object,
-        IDisposableObject
+    public class Disposable : object, IDisposable
     {
         #region Properties
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets a value indicating whether this object is in the process of disposing.
+        /// </summary>
         public bool IsDisposing => Interlocked.CompareExchange(ref disposeStage, DisposalStarted, DisposalStarted) == DisposalStarted;
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets a value indicating whether this object has been disposed.
+        /// </summary>
         public bool IsDisposed => Interlocked.CompareExchange(ref disposeStage, DisposalComplete, DisposalComplete) == DisposalComplete;
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Gets a value indicating whether this object has been disposed or is in the process of being disposed.
+        /// </summary>
         public bool IsDisposedOrDisposing => Interlocked.CompareExchange(ref disposeStage, DisposalNotStarted, DisposalNotStarted) != DisposalNotStarted;
 
         /// <summary>
@@ -41,12 +45,20 @@ namespace DisposableHelpers
         // see the constants defined above for valid values
         private int disposeStage;
 
+#nullable enable
+        private readonly Action<bool>? dispose;
+#nullable disable
+
         #endregion
 
         #region Events
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Occurs when this object is about to be disposed.
+        /// </summary>
+#nullable enable
         public event EventHandler? Disposing;
+#nullable disable
 
         #endregion
 
@@ -61,7 +73,15 @@ namespace DisposableHelpers
         }
 
         /// <summary>
-        /// Finalizes an instance of the Disposable class.
+        /// Creates an instance of <see cref="Disposable"/> class with action on dispose.
+        /// </summary>
+        public Disposable(Action<bool> dispose)
+        {
+            this.dispose = dispose;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="Disposable"/> class.
         /// </summary>
         ~Disposable()
         {
@@ -85,33 +105,10 @@ namespace DisposableHelpers
             OnDisposing();
             Disposing = null;
 
+            dispose?.Invoke(true);
             Dispose(true);
-            DisposeAsync(true);
             MarkAsDisposed();
         }
-
-#if NETSTANDARD2_1_OR_GREATER
-        /// <summary>
-        /// Disposes of this object, if it hasn't already been disposed.
-        /// </summary>
-        /// <returns>
-        /// The <see cref="ValueTask"/> of the dispose operation.
-        /// </returns>
-        public async ValueTask DisposeAsync()
-        {
-            if (Interlocked.CompareExchange(ref disposeStage, DisposalStarted, DisposalNotStarted) != DisposalNotStarted)
-            {
-                return;
-            }
-
-            OnDisposing();
-            Disposing = null;
-
-            Dispose(true);
-            await DisposeAsync(true);
-            MarkAsDisposed();
-        }
-#endif
 
         /// <summary>
         /// Verifies that this object is not in the process of disposing, throwing an exception if it is.
@@ -156,7 +153,175 @@ namespace DisposableHelpers
         {
         }
 
+        /// <summary>
+        /// Raises the <see cref="Disposing"/> event.
+        /// </summary>
+        protected virtual void OnDisposing()
+        {
+            Disposing?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// Marks this object as disposed without running any other dispose logic.
+        /// </summary>
+        /// <remarks>
+        /// Use this method with caution. It is helpful when you have an object that can be disposed in multiple fashions, such as through a <c>CloseAsync</c> method.
+        /// </remarks>
+        protected void MarkAsDisposed()
+        {
+            GC.SuppressFinalize(this);
+            _ = Interlocked.Exchange(ref disposeStage, DisposalComplete);
+        }
+
+        #endregion
+    }
+
 #if NETSTANDARD2_1_OR_GREATER
+    /// <summary>
+    /// Contains all methods for performing proper <see cref="IAsyncDisposable"/> operations.
+    /// </summary>
+    public class AsyncDisposable : object, IAsyncDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether this object is in the process of disposing.
+        /// </summary>
+        public bool IsDisposing => Interlocked.CompareExchange(ref disposeStage, DisposalStarted, DisposalStarted) == DisposalStarted;
+
+        /// <summary>
+        /// Gets a value indicating whether this object has been disposed.
+        /// </summary>
+        public bool IsDisposed => Interlocked.CompareExchange(ref disposeStage, DisposalComplete, DisposalComplete) == DisposalComplete;
+
+        /// <summary>
+        /// Gets a value indicating whether this object has been disposed or is in the process of being disposed.
+        /// </summary>
+        public bool IsDisposedOrDisposing => Interlocked.CompareExchange(ref disposeStage, DisposalNotStarted, DisposalNotStarted) != DisposalNotStarted;
+
+        /// <summary>
+        /// Gets the object name, for use in any <see cref="ObjectDisposedException"/> thrown by this object.
+        /// </summary>
+        /// <remarks>
+        /// Subclasses can override this property if they would like more control over the object name appearing in any <see cref="ObjectDisposedException"/>
+        /// thrown by this <see cref="Disposable"/>. This can be particularly useful in debugging and diagnostic scenarios.
+        /// </remarks>
+        /// <value>
+        /// The object name, which defaults to the class name.
+        /// </value>
+        protected virtual string ObjectName => GetType().FullName;
+
+        private const int DisposalNotStarted = 0;
+        private const int DisposalStarted = 1;
+        private const int DisposalComplete = 2;
+
+        // see the constants defined above for valid values
+        private int disposeStage;
+
+#nullable enable
+        private readonly Func<bool, ValueTask>? dispose;
+#nullable disable
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Occurs when this object is about to be disposed.
+        /// </summary>
+#nullable enable
+        public event EventHandler? Disposing;
+#nullable disable
+
+        #endregion
+
+        #region Initializers
+
+        /// <summary>
+        /// Creates an instance of <see cref="AsyncDisposable"/> class.
+        /// </summary>
+        public AsyncDisposable()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="AsyncDisposable"/> class with action on dispose.
+        /// </summary>
+        public AsyncDisposable(Func<bool, ValueTask> dispose)
+        {
+            this.dispose = dispose;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="AsyncDisposable"/> class.
+        /// </summary>
+        ~AsyncDisposable()
+        {
+            DisposeAsync(false);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Disposes of this object, if it hasn't already been disposed.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ValueTask"/> of the dispose operation.
+        /// </returns>
+        public async ValueTask DisposeAsync()
+        {
+            if (Interlocked.CompareExchange(ref disposeStage, DisposalStarted, DisposalNotStarted) != DisposalNotStarted)
+            {
+                return;
+            }
+
+            OnDisposing();
+            Disposing = null;
+
+            if (dispose != null)
+            {
+                await dispose.Invoke(true);
+            }
+            await DisposeAsync(true);
+            MarkAsDisposed();
+        }
+
+        /// <summary>
+        /// Verifies that this object is not in the process of disposing, throwing an exception if it is.
+        /// </summary>
+        protected void VerifyNotDisposing()
+        {
+            if (IsDisposing)
+            {
+                throw new ObjectDisposedException(ObjectName);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that this object has not been disposed, throwing an exception if it is.
+        /// </summary>
+        protected void VerifyNotDisposed()
+        {
+            if (IsDisposed)
+            {
+                throw new ObjectDisposedException(ObjectName);
+            }
+        }
+
+        /// <summary>
+        /// Verifies that this object is not being disposed or has been disposed, throwing an exception if either of these are true.
+        /// </summary>
+        protected void VerifyNotDisposedOrDisposing()
+        {
+            if (IsDisposedOrDisposing)
+            {
+                throw new ObjectDisposedException(ObjectName);
+            }
+        }
+
         /// <summary>
         /// Allows subclasses to provide dispose logic.
         /// </summary>
@@ -170,7 +335,6 @@ namespace DisposableHelpers
         {
             return default;
         }
-#endif
 
         /// <summary>
         /// Raises the <see cref="Disposing"/> event.
@@ -194,4 +358,5 @@ namespace DisposableHelpers
 
         #endregion
     }
+#endif
 }
---
+++ DisposableHelpersTest/DisposableTest.cs
@@ -0,0 +1,218 @@
+using DisposableHelpers;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DisposableHelpersTest
+{
+    public class DisposeTest : Disposable
+    {
+        public int DisposeCallCount { get; private set; } = 0;
+
+        protected override void Dispose(bool disposing)
+        {
+            Thread.Sleep(1000);
+            if (disposing)
+            {
+                DisposeCallCount++;
+            }
+            base.Dispose(disposing);
+        }
+
+        [Fact]
+        public async void Normal()
+        {
+            int disposingCallCount = 0;
+            var dispose = new DisposeTest();
+            dispose.Disposing += (s, e) =>
+            {
+                disposingCallCount++;
+            };
+            Assert.Equal(0, dispose.DisposeCallCount);
+            Assert.Equal(0, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.False(dispose.IsDisposedOrDisposing);
+            Task run = Task.Run(async delegate
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                dispose.Dispose();
+            });
+            async void runVoid() => await run;
+            runVoid();
+            await Task.Delay(100);
+            Assert.Equal(0, dispose.DisposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.True(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposedOrDisposing);
+            await Task.Delay(1500);
+            Assert.Equal(1, dispose.DisposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.True(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposed);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposedOrDisposing);
+            runVoid();
+            await Task.Delay(1500);
+            Assert.Equal(1, dispose.DisposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+        }
+
+        [Fact]
+        public async void Anonimous()
+        {
+            int disposeCallCount = 0;
+            int disposingCallCount = 0;
+            var dispose = new Disposable(disposing =>
+            {
+                Thread.Sleep(1000);
+                if (disposing)
+                {
+                    disposeCallCount++;
+                }
+            });
+            dispose.Disposing += (s, e) =>
+            {
+                disposingCallCount++;
+            };
+            Assert.Equal(0, disposeCallCount);
+            Assert.Equal(0, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.False(dispose.IsDisposedOrDisposing);
+            Task run = Task.Run(async delegate
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                dispose.Dispose();
+            });
+            async void runVoid() => await run;
+            runVoid();
+            await Task.Delay(100);
+            Assert.Equal(0, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.True(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            await Task.Delay(1500);
+            Assert.Equal(1, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.True(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            runVoid();
+            await Task.Delay(1500);
+            Assert.Equal(1, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+        }
+    }
+
+    public class AsyncDisposeTest : AsyncDisposable
+    {
+        public int DisposedCallCount { get; private set; } = 0;
+
+        protected override async ValueTask DisposeAsync(bool disposing)
+        {
+            await Task.Delay(1000);
+            if (disposing)
+            {
+                DisposedCallCount++;
+            }
+            await base.DisposeAsync(disposing);
+        }
+
+        [Fact]
+        public async void Normal()
+        {
+            int disposingCallCount = 0;
+            var dispose = new AsyncDisposeTest();
+            dispose.Disposing += (s, e) =>
+            {
+                disposingCallCount++;
+            };
+            Assert.Equal(0, dispose.DisposedCallCount);
+            Assert.Equal(0, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.False(dispose.IsDisposedOrDisposing);
+            Task run = Task.Run(async delegate
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                await dispose.DisposeAsync();
+            });
+            async void runVoid() => await run;
+            runVoid();
+            await Task.Delay(100);
+            Assert.Equal(0, dispose.DisposedCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.True(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposedOrDisposing);
+            await Task.Delay(1500);
+            Assert.Equal(1, dispose.DisposedCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.True(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposed);
+            Assert.Throws<ObjectDisposedException>(dispose.VerifyNotDisposedOrDisposing);
+            runVoid();
+            await Task.Delay(1500);
+            Assert.Equal(1, dispose.DisposedCallCount);
+            Assert.Equal(1, disposingCallCount);
+        }
+
+        [Fact]
+        public async void Anonimous()
+        {
+            int disposeCallCount = 0;
+            int disposingCallCount = 0;
+            var dispose = new AsyncDisposable(async disposing =>
+            {
+                await Task.Delay(1000);
+                if (disposing)
+                {
+                    disposeCallCount++;
+                }
+            });
+            dispose.Disposing += (s, e) =>
+            {
+                disposingCallCount++;
+            };
+            Assert.Equal(0, disposeCallCount);
+            Assert.Equal(0, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.False(dispose.IsDisposedOrDisposing);
+            Task run = Task.Run(async delegate
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+                await dispose.DisposeAsync();
+            });
+            async void runVoid() => await run;
+            runVoid();
+            await Task.Delay(100);
+            Assert.Equal(0, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.False(dispose.IsDisposed);
+            Assert.True(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            await Task.Delay(1500);
+            Assert.Equal(1, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+            Assert.True(dispose.IsDisposed);
+            Assert.False(dispose.IsDisposing);
+            Assert.True(dispose.IsDisposedOrDisposing);
+            runVoid();
+            await Task.Delay(1500);
+            Assert.Equal(1, disposeCallCount);
+            Assert.Equal(1, disposingCallCount);
+        }
+    }
+}
